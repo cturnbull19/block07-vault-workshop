7310190125344ac80a39f528818e81f78c8c5d0d
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2023-11-06 12:30:35 -0500

    steps 4 and 5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
-const message = 'You have receieved this message because you have been chosen to open an important vault. Here is the secrete combination:'
+const message1 = 'You have receieved this message because you have been chosen to open an important vault.'
+const message2 = 'Here is the secrete combination:'
 
 
 /*Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator
@@ -15,5 +16,9 @@ console.log(x, + y, + z,)
 //Add comments throughout the code to explain each step in the process.
 
 
-
 //Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+
+//I want to first create a dialog box that will display the original message.
+alert(message1)
+
+alert(x, + y, + z,)
