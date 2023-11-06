47c999648bcf4579dd22c6231cda739c30e0694c
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2023-11-06 12:25:59 -0500

    steps 1-3

---
+++ index.js
@@ -0,0 +1,19 @@
+//Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+const message = 'You have receieved this message because you have been chosen to open an important vault. Here is the secrete combination:'
+
+
+/*Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator
+Each must equal one of the three codes in the combination. */
+let x = 6+5;
+let y = 6*5;
+let z = 6-5;
+
+//if i just used x + y + z without the commas, it would've added the values together
+console.log(x, + y, + z,)
+
+
+//Add comments throughout the code to explain each step in the process.
+
+
+
+//Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
